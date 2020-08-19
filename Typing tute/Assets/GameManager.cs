d29c121a405a9f6ecc6689427978a877b6410af8
Kyla <kcanares@student.unimelb.edu.au>
2020-08-19 16:59:53 +1000

    added bg

--- Typing tute/Assets/GameManager.cs
+++ Typing tute/Assets/GameManager.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    private float MAX_TEXT_WIDTH = 220;
-    private int TEXTBOX_PADDING = 25;
+    private float MAX_TEXT_WIDTH = 180;
+    private int TEXTBOX_PADDING = 15;
 
     public int msgCount;
     public GameObject chatPanel, playerWrapper, replyWrapper;
@@ -70,15 +71,21 @@ public class GameManager : MonoBehaviour
         if (textWidth > MAX_TEXT_WIDTH)
         {
             textWidth = MAX_TEXT_WIDTH;
+            leftJustify(newMsg);
         }
 
         float origWrapperWidth = newMsg.GetComponent<RectTransform>().sizeDelta.x;
 
         // set background coloured container size to textbox size
-        newMsg.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(textWidth + 10, textHeight + 5);
+        newMsg.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(textWidth + 15, textHeight + 15);
 
         // set wrapper's dimensions to text height
         newMsg.GetComponent<RectTransform>().sizeDelta = new Vector2(origWrapperWidth, textHeight + TEXTBOX_PADDING);
+    }
+
+    private void leftJustify(GameObject newMsg)
+    {
+        newMsg.transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().alignment = TextAlignmentOptions.TopLeft;
     }
 
 }
