cdc25532dfd8d01848b3d6eb9862b4a3d7a76283
Cynthia Joseph <cynthia.jo99@gmail.com>
2020-08-19 21:17:33 +1000

    Countdown-ver

---
+++ Typing tute-count/Assets/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private float MAX_TEXT_WIDTH = 180;
+    private int TEXTBOX_PADDING = 15;
+
+    public int msgCount;
+    public GameObject chatPanel, playerWrapper, replyWrapper;
+    public InputField chatBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        msgCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chatBox.text != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessageToChat();
+                chatBox.text = "";
+            }
+        }
+        // activates chatbox if it's not focused
+        else if (!chatBox.isFocused)
+        {
+            chatBox.ActivateInputField();
+        }
+
+    }
+
+    public void SendMessageToChat()
+    {
+        GameObject newMsg = new GameObject();
+
+        if (msgCount % 2 == 0)
+        {
+            newMsg = Instantiate(playerWrapper, chatPanel.transform);
+        }
+        else
+        {
+            newMsg = Instantiate(replyWrapper, chatPanel.transform);
+        }
+
+        // get child text object of the wrapper and set text
+        newMsg.transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = chatBox.text;
+
+        resizeMsg(newMsg);
+
+        // newMessage.textObject.text = newMessage.text;
+        msgCount += 1;
+    }
+
+    private void resizeMsg(GameObject newMsg)
+    {
+        float textHeight = newMsg.transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().preferredHeight;
+
+        float textWidth = newMsg.transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().preferredWidth;
+
+        if (textWidth > MAX_TEXT_WIDTH)
+        {
+            textWidth = MAX_TEXT_WIDTH;
+            leftJustify(newMsg);
+        }
+
+        float origWrapperWidth = newMsg.GetComponent<RectTransform>().sizeDelta.x;
+
+        // set background coloured container size to textbox size
+        newMsg.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(textWidth + 15, textHeight + 15);
+
+        // set wrapper's dimensions to text height
+        newMsg.GetComponent<RectTransform>().sizeDelta = new Vector2(origWrapperWidth, textHeight + TEXTBOX_PADDING);
+    }
+
+    private void leftJustify(GameObject newMsg)
+    {
+        newMsg.transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().alignment = TextAlignmentOptions.TopLeft;
+    }
+
+}
+
+[System.Serializable]
+public class Message
+{
+    public string text;
+    public Text textObject;
+
+}
---
+++ Typing tute-count/Assets/RightJustifiedResizer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+// gets textbox width to be as small as possible so the text can be 
+// left justified without looking weird
+public class RightJustifiedResizer : MonoBehaviour
+{
+    public float MAX_TEXT_WIDTH = 220;
+    private float textWidth;
+    private static float textHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textWidth = this.GetComponent<TMPro.TextMeshProUGUI>().preferredWidth;
+        textHeight = this.GetComponent<TMPro.TextMeshProUGUI>().preferredHeight;
+
+        if (textWidth > MAX_TEXT_WIDTH)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(MAX_TEXT_WIDTH, textHeight);
+        } else
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(textWidth, textHeight);
+        }
+        UnityEngine.Debug.Log(textHeight);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
