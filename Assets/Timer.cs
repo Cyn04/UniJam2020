d57a436ad60835a3cf3092cbdbab828ab7ca8aa5
Kyla C <canares.kyla@gmail.com>
2020-08-22 18:56:14 +1000

    added timer text and instructions

---
+++ Assets/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Diagnostics;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI uiText = null;
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+
+    void Start()
+    {
+        timer = mainTimer;
+
+    }
+
+    void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("F");
+        }
+
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0.00";
+            timer = 0.0f;
+
+        }
+
+    }
+
+}
